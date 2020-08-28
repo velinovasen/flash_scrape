5601407eb3e5df2aae1bf0f968ca8beb8fabad94
velinovasen <velinovasen@users.noreply.github.com>
2020-08-28 20:18:13 +0300

    'cc'

--- demo2_test.py
+++ demo2_test.py
@@ -1,5 +1,6 @@
 from contextlib import suppress
 from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import requests
 import time
@@ -13,10 +14,11 @@ WEB_LINKS = {
 
 data = []
 
-
-driver = Chrome()
+options = Options()
+options.headless = True
+driver = Chrome(options=options, executable_path='C:\Windows\chromedriver.exe')
 driver.get(WEB_LINKS["Football"])
-time.sleep(2)
+
 
 while True:
     with suppress(Exception):
@@ -25,20 +27,66 @@ while True:
 
 html = driver.execute_script("return document.documentElement.outerHTML;")
 soup = BeautifulSoup(html, 'html.parser')
-rows = soup.find_all('div')
-items = []
-for row in rows:
-    if row.has_attr('class'):
-        with suppress(IndexError):
-            if "event__match event__match--oneLine" in row['class']:
-                items.append(row.text)
-print(rows)
-print(items)
+matches = soup.find_all(class_=re.compile("event__match"))
 
-# for x in range(len(rows)):
-#     if len(rows[x]) > best:
-#         best = x
-#     cls = rows[x].attrs.get("class")
-#     print(rows[x])
-#
+for row in matches:
+    token = str(row)
+    tokens = token.split('div ')
+    items = {"time": None, "home_team": None, "away_team": None, "home_odd": {"old": None, "new": None},
+             "draw_odd": {"old": None, "new": None}, "away_odd": {"old": None, "new": None}}
+    for el in tokens:
+        if 'class="event__time' in el:
+            pattern = r'\d+[:]\d{2}'
+            time = re.search(pattern, el)
+            items["time"] = time.group()
+        elif 'participant--home' in el:
+            pattern = r'\"\>(.+\(|\<\/[d]|.+\<)'
+            home_search = re.search(pattern, el)
+            home_team = ''
+            if home_search.group()[-1] == '(':
+                home_team = home_search.group()[2:-2]
+            else:
+                home_team = home_search.group()[2:-1]
+            items["home_team"] = home_team
+        elif 'participant--away' in el:
+            pattern = r'\"\>(.+\(|.+\<)'
+            away_search = re.search(pattern, el)
+            away_team = ''
+            if away_search.group()[-1] == '(':
+                away_team = away_search.group()[2:-2]
+            else:
+                away_team = away_search.groups()[2:-1]
+            items["away_team"] = away_team
+        elif '<span alt=' in el:
+            old_odd_pattern = r"\=\"(\d+\.\d+)\["
+            new_odd_pattern = r"\"\>(\d+\.\d+)\<"
+            old_odd_search = re.search(old_odd_pattern, el)
+            new_odd_search = re.search(new_odd_pattern, el)
+    print(tokens)
 
+    result_to_print = f""
+    for key, value in items.items():
+        result_to_print += f"---[{key}---{value}]---"
+    print(result_to_print)
+print(len(matches))
+
+# if len(tokens) == 6:
+#     if tokens.index(el) == 4:
+#         items["home_odd"]["old"] = old_odd_search.group()
+#         items["home_odd"]["new"] = new_odd_search.group()
+#     elif tokens.index(el) == 5:
+#         items["draw_odd"]["old"] = old_odd_search.group()
+#         items["draw_odd"]["new"] = new_odd_search.group()
+#     elif tokens.index(el) == 6:
+#         items["away_odd"]["old"] = old_odd_search.group()
+#         items["away_odd"]["new"] = new_odd_search.group()
+# elif len(tokens) == 5:
+#     if tokens.index(el) == 3:
+#         items["home_odd"]["old"] = old_odd_search.group()
+#         items["home_odd"]["new"] = new_odd_search.group()
+#     elif tokens.index(el) == 4:
+#         items["draw_odd"]["old"] = old_odd_search.group()
+#         items["draw_odd"]["new"] = new_odd_search.group()
+#     elif tokens.index(el) == 5:
+#         items["away_odd"]["old"] = old_odd_search.group()
+#         items["away_odd"]["new"] = new_odd_search.group()
