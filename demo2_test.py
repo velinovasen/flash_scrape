c37a1858012e8e2f1bb3d340246b1b676d4349b4
velinovasen <velinovasen@users.noreply.github.com>
2020-08-29 10:35:54 +0300

    'fixed_time'

--- demo2_test.py
+++ demo2_test.py
@@ -11,7 +11,6 @@ WEB_LINKS = {
     "Tennis": "https://www.flashscore.com/tennis/"
 }
 
-
 data = []
 
 options = Options()
@@ -19,12 +18,16 @@ options.headless = True
 driver = Chrome(options=options, executable_path='C:\Windows\chromedriver.exe')
 driver.get(WEB_LINKS["Football"])
 
-
 while True:
     with suppress(Exception):
         driver.find_element_by_css_selector("#live-table > div.tabs > div.tabs__group > div:nth-child(4) > div").click()
         break
 
+while True:
+    with suppress(Exception):
+        driver.find_element_by_css_selector("#live-table > div.tabs > div.calendar > div:nth-child(3) > div").click()
+        break
+time.sleep(3)
 html = driver.execute_script("return document.documentElement.outerHTML;")
 soup = BeautifulSoup(html, 'html.parser')
 matches = soup.find_all(class_=re.compile("event__match"))
@@ -34,21 +37,32 @@ for row in matches:
     tokens = token.split('div ')
     items = {"time": None, "home_team": None, "away_team": None, "home_odd": None,
              "draw_odd": None, "away_odd": None}
+    #print(tokens)
     for el in tokens:
         if 'class="event__time' in el:
+            #print(el)
             pattern = r'\d+[:]\d{2}'
             time = re.search(pattern, el)
             items["time"] = time.group()
+
+        elif '"event__stage--block">' in el:
+            if 'Cancelled' in el:
+                items["time"] = "Cancelled"
+            elif 'Postponed' in el:
+                items["time"] = "Postponed"
+
         elif 'participant--home' in el:
             pattern = r'\"\>(.{1,})\/'
             home_search = re.search(pattern, el)
             home_team = home_search.group()[2:-2]
             items["home_team"] = home_team
+
         elif 'participant--away' in el:
             pattern = r'\"\>(.{1,})\/'
             away_search = re.search(pattern, el)
             away_team = away_search.group()[2:-2]
             items["away_team"] = away_team
+
         elif '<span alt=' in el:
             odd_pattern = r"\"\>(\d+\.\d+)\<"
             odd_search = re.search(odd_pattern, el)
@@ -62,7 +76,7 @@ for row in matches:
                 odd = odd_search.group()[2:-1]
                 items["away_odd"] = odd
 
-    #print(tokens)
+    # print(tokens)
 
     result_to_print = f""
     for key, value in items.items():
