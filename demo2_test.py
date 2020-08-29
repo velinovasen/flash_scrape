d47233da29490864da6a5a700d1dd86aaab8851e
velinovasen <velinovasen@users.noreply.github.com>
2020-08-29 16:51:10 +0300

    'added_db'

--- demo2_test.py
+++ demo2_test.py
@@ -1,31 +1,40 @@
-from contextlib import suppress
-from selenium.webdriver import Chrome
-from selenium.webdriver.chrome.options import Options
-from bs4 import BeautifulSoup
-import requests
-import time
 import re
+import sqlite3
+import time
+from contextlib import suppress
+
+from bs4 import BeautifulSoup
+from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.options import Options
 
 WEB_LINKS = {
-    "Football": "https://www.flashscore.com/football/",
-    "Tennis": "https://www.flashscore.com/tennis/"
-}
+        "Football": "https://www.flashscore.com/football/",
+        "Tennis": "https://www.flashscore.com/tennis/"
+    }
 
-data = []
+all_data = []
+
+conn = sqlite3.connect('test_bin.db')
+c = conn.cursor()
+c.execute('DROP TABLE IF EXISTS allGames')
+c.execute('CREATE TABLE allGames(time TEXT, home_team TEXT, '
+              'away_team TEXT, home_odd REAL, draw_odd REAL, away_odd REAL)')
 
 options = Options()
 options.headless = True
-driver = Chrome(options=options, executable_path='C:\Windows\chromedriver.exe')
+driver = Firefox(options=options, executable_path='C:\Windows\geckodriver.exe')
 driver.get(WEB_LINKS["Football"])
 
 while True:
     with suppress(Exception):
-        driver.find_element_by_css_selector("#live-table > div.tabs > div.tabs__group > div:nth-child(4) > div").click()
+        driver.find_element_by_css_selector(
+            "#live-table > div.tabs > div.tabs__group > div:nth-child(4) > div").click()
         break
 
 while True:
     with suppress(Exception):
-        driver.find_element_by_css_selector("#live-table > div.tabs > div.calendar > div:nth-child(3) > div").click()
+        driver.find_element_by_css_selector(
+            "#live-table > div.tabs > div.calendar > div:nth-child(3) > div").click()
         break
 time.sleep(3)
 html = driver.execute_script("return document.documentElement.outerHTML;")
@@ -35,12 +44,12 @@ matches = soup.find_all(class_=re.compile("event__match"))
 for row in matches:
     token = str(row)
     tokens = token.split('div ')
-    items = {"time": None, "home_team": None, "away_team": None, "home_odd": None,
-             "draw_odd": None, "away_odd": None}
-    #print(tokens)
+    items = {"time": None, "home_team": None, "away_team": None, "home_odd": "",
+             "draw_odd": "", "away_odd": ""}
+    # print(tokens)
     for el in tokens:
         if 'class="event__time' in el:
-            #print(el)
+            # print(el)
             pattern = r'\d+[:]\d{2}'
             time = re.search(pattern, el)
             items["time"] = time.group()
@@ -76,10 +85,15 @@ for row in matches:
                 odd = odd_search.group()[2:-1]
                 items["away_odd"] = odd
 
+    c.execute('INSERT INTO allGames (time, home_team, away_team, home_odd, draw_odd, away_odd)'
+                   ' VALUES (?, ?, ?, ?, ?, ?)', (items["time"], items["home_team"], items["away_team"],
+                                                  items["home_odd"], items["draw_odd"], items["away_odd"]))
+    conn.commit()
+
     # print(tokens)
 
     result_to_print = f""
     for key, value in items.items():
         result_to_print += f"---[{key}---{value}]---"
     print(result_to_print)
-print(len(matches))
+
